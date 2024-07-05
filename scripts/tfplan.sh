409aa19e59773a98b6c3d99d25283189cfd4c1bf
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-07-05 09:43:51 +0200

    test9 (#12)

--- scripts/tfplan.sh
+++ scripts/tfplan.sh
@@ -3,8 +3,8 @@ echo "+++++++++++++++++++++++++++"
 echo "+ Executing Terraform plan"
 echo "+++++++++++++++++++++++++++"
 
-export BTP_USERNAME=$btp-authentication_user
-export BTP_PASSWORD=$btp-authentication_password
+#export BTP_USERNAME=$btp-authentication_user
+#export BTP_PASSWORD=$btp-authentication_password
 
 ./tools/terraform -chdir="setup_subaccount" init
-./tools/terraform -chdir="setup_subaccount" plan -var globalaccount=$globalaccount-subdomain
+#./tools/terraform -chdir="setup_subaccount" plan -var globalaccount=$globalaccount-subdomain
